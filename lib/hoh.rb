dcf5ad8600cf98b8c5b9cdeb70e83031f25fe6c2
April Escobar <aprilfaith.e@gmail.com>
2020-01-15 04:19:02 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,15 +43,20 @@ def naming_system
   # HoH all the way to Species!
   
   starting_node = {
+    label: "Kingdom",
     sub_category: {
+      label: "Phylum",
       sub_category: {
-        sub_category:{
+        label: "Class",
+        sub_category: {
           label: "Order",
           sub_category: {
+            label: "Family",
             sub_category: {
               label: "Genus",
               sub_category: {
-                    label: "Species"
+                label: "Species",
+                sub_category: nil
               }
             }
           }
