9a8d34194d1d940d31c073be51ec3959722a5794
Jared Fleming <jaredmf10@gmail.com>
2020-01-18 23:45:31 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -4,7 +4,7 @@
 #
 # The tests will guide your construction.
 #
-# PORTION_1 = {
+#PORTION_1 = {
 #   label: "Kingdom",
 #   sub_category: {
 #     label: "Phylum",
@@ -13,18 +13,18 @@
 #     }
 #   }
 # }
-# 
+#PORTION_1[:label]
 # PORTION_2 = {
 #   label: "Order"
 # }
-# 
+
 # PORTION_3 = {
 #   label: "Family",
 #   sub_category: {
 #     label: "Genus",
 #   }
 # }
-# 
+
 # PORTION_4 = {
 #   label: "Species",
 #   sub_category: nil
@@ -41,5 +41,35 @@ def naming_system
   #  Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
-
+  naming_system = {
+    label: "Kingdom",
+       sub_category: {
+         label: "Phylum",
+         sub_category: {
+           label: "Class",
+          sub_category: {
+            label:"Order",
+          sub_category: {
+            label: "Family",
+          sub_category: {
+            label: "Genus",
+          sub_category: {
+            label: "Species",
+          sub_category: nil
+          }
+          }
+          }
+          }
+          }
+        }
+  }
+#[:sub_category][:sub_category][:sub_category][:label]
+#PORTION_1[:sub_category][:sub_category][:sub_category] = PORTION_2
+#end
+#person = {name: "Tony", age: 31}
+#store = {
+#  address: "123 Fake Street",
+#  employees: person
+#}
+#store[:employees][:person][:name] = "Jared"
 end
